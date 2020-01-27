81968c79777d78f3c808e5de76c63b793ed58f8b
JoseLSilva <jose@silva.app>
2020-01-27 11:15:23 -0500

    Add env and config next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 const withCSS = require("@zeit/next-css");
 
-module.exports = withCSS();
+module.exports = withCSS({
+  env: {
+    LANG: "en",
+    TITLE: "Blog Nextjs",
+    DESCRIPTION: "Blog developed in Next.js"
+  },
+  devIndicators: {
+    autoPrerender: false
+  },
+  poweredByHeader: false
+});
