cdda7a5daf8235f1e93fcb062048a00bc6dd5cad
JoseLSilva <jose@silva.app>
2020-01-31 18:15:27 -0500

    Clean service worker

--- public/sw.js
+++ public/sw.js
@@ -3,21 +3,13 @@ console.log("service worker");
 var CACHE_NAME = "blog-next";
 var urlsToCache = ["/"];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", event => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
-  );
+  const preLoaded = caches
+    .open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache));
+  event.waitUntil(preLoaded);
 });
-
-// self.addEventListener("install", event => {
-//   const preLoaded = caches.open(CACHE_NAME)
-//       .then(cache => cache.addAll(urlsToCache))
-//   event.waitUntil(preLoaded);
-// });
 
 self.addEventListener("fetch", event => {
   const response = caches
